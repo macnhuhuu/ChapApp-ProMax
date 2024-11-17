7fc33eb0117bb80e439d9e4a25f4029e4e31dd99
macnhuhuu <huusaitokai@gmail.com>
2024-11-17 17:06:33 +0700

    sdas

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,15 +1,16 @@
 import { currentProfile } from '@/lib/current-profile'
 import { db } from '@/lib/db'
 import { redirectToSignIn } from '@clerk/nextjs'
-
 import { redirect } from 'next/navigation'
 
 interface InviteCodePageProps {
   params: {
-    inviteCode: string
-  }
+    inviteCode: string;
+  };
+  // Nếu cần, bạn có thể thêm các kiểu dữ liệu cho các props khác tại đây
 }
 
+// Lưu ý là params sẽ phải là một đối tượng có thể resolve đúng kiểu
 const InviteCodePage = async ({ params }: InviteCodePageProps) => {
   const profile = await currentProfile()
 
@@ -17,7 +18,7 @@ const InviteCodePage = async ({ params }: InviteCodePageProps) => {
     return redirectToSignIn()
   }
 
-  if (!params.inviteCode) {
+  if (!params?.inviteCode) { // Kiểm tra params.inviteCode
     return redirect('/')
   }
 
